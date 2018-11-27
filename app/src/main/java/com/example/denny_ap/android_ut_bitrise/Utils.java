1a7af481ba80f1a9fa8314d0376aa4607dd4f919
Denny Matthijs <denny.matthijs@student.ap.be>
2018-11-27 11:45:02 +0100

    First commit

---
+++ app/src/main/java/com/example/denny_ap/android_ut_bitrise/Utils.java
@@ -0,0 +1,9 @@
+package com.example.denny_ap.android_ut_bitrise;
+
+public class Utils {
+
+    public static int addNumbers(int first, int second) {
+        return first + second;
+    }
+
+}
---
+++ app/src/test/java/com/example/denny_ap/android_ut_bitrise/ExampleUnitTest.java
@@ -0,0 +1,23 @@
+package com.example.denny_ap.android_ut_bitrise;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+
+    @Test
+    public void add_isCorrect() throws Exception {
+        assertEquals(4, Utils.addNumbers(2 , 2));
+    }
+
+    @Test
+    public void add_twodigits_isCorrect() throws Exception {
+        assertEquals(44, Utils.addNumbers(22 , 22));
+    }
+
+    @Test
+    public void add_big_isCorrect() throws Exception {
+        assertEquals(4444, Utils.addNumbers(2222 , 2222));
+    }
+
+}
